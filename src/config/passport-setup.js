f341dbeb322868d5029b615c4e034a3838b83ba1
venetrius <gergelygjuhasz@gmail.com>
2019-06-28 14:52:45 -0600

    add passport

---
+++ src/config/passport-setup.js
@@ -0,0 +1,59 @@
+const passport = require('passport');
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+require('dotenv').config(); 
+
+let dataHelpers = null;
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => 
+  {
+    dataHelpers.getUserById(id, (user) => done(null, user))
+  } 
+);
+
+const createUserProfile = function(profile){
+    const userProfile = {
+        token : profile.id,
+        photo : profile.photos.length > 0 ? profile.photos[0].value : null,
+        first_name: profile.name.givenName,
+        last_name: profile.name.familyName,
+        email_address: profile.name.givenName + '@' + profile.name.familyName + '.com',
+        'linkedin-link':'https://www.linkedin.com'
+    }
+    return userProfile;
+}
+
+const setUpLinkedinPassport = function (dataHelpersParam) {
+  dataHelpers = dataHelpersParam;
+  passport.use(
+    new LinkedInStrategy({
+      // options for google strategy
+      clientID: process.env.LINKEDIN_CLIENT_ID,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+      callbackURL: 'http://127.0.0.1:8081/auth/linkedin/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+      // passport callback function
+      dataHelpers.getUserByToken(
+        profile.id,
+        function (error, user) {
+          if (user) {
+            done(null, user);
+          } else {
+            const userProfile = createUserProfile(profile);
+            dataHelpers.createUser(
+              userProfile,
+              function (err, user) {
+                done(null, user[0]);
+              }
+            );
+          }
+        }
+      )
+    })
+  );
+}
+
+module.exports = setUpLinkedinPassport;
